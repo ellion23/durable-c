ad96c50a251984fed0ef4388d0a1a44a6b0988ad
ellion23 <rubcinskija@gmail.com>
2023-05-29 03:05:41 +0700

    upd

--- Trash/BinaryFwithMatrix.c
+++ Trash/BinaryFwithMatrix.c
@@ -1,45 +1,45 @@
-//Функцию создания двоичного файла, содержащего матрицу целых чисел размерности n*n.
+// Функцию создания двоичного файла, содержащего матрицу целых чисел размерности n*n.
 // Значение n задается с клавиатуры. Элементы матрицы - целые случайные числа.
 //
-//- Функция печати содержимого файла.
+// Функция печати содержимого файла.
 //
-//- Функцию вычисляющую сумму элементов заданного столбца. Всю матрицу при этом просматривать нельзя.
-// Created by user on 23.02.2023.
+// Функцию вычисляющую сумму элементов заданного столбца. Всю матрицу при этом просматривать нельзя.
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int** binary_file_w_matrix(char* name, int n){
-    FILE *binary_file = fopen(name,"wb");
+int **binary_file_w_matrix(char *name, int n) {
+    FILE *binary_file = fopen(name, "wb");
 
     if (binary_file == NULL) {
         printf("Error. Cannot create file. \n");
         system("pause");
     }
     int **x, i, j;
-    x=(int**)malloc(sizeof(int*)*n);
-    for(i = 0; i < n; i++)
-        x[i] = (int*)malloc(sizeof(int)*n);
+    x = (int **) malloc(sizeof(int *) * n);
+    for (i = 0; i < n; i++)
+        x[i] = (int *) malloc(sizeof(int) * n);
 
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            x[i][j] = rand()%40;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            x[i][j] = rand() % 40;
             fwrite(&x[i][j], sizeof(int), 1, binary_file);
         }
     }
     fclose(binary_file);
     return x;
 }
-void print_file(char* name, int n){
-    FILE *binary_file = fopen(name,"rb");
+
+void print_file(char *name, int n) {
+    FILE *binary_file = fopen(name, "rb");
     if (binary_file == NULL) {
         printf("Error. Cannot find file. \n");
         system("pause");
-        return ;
+        return;
     }
     int number;
-    for (int i = 0; i < n; i++){
-        for (int j; j < n; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j; j < n; j++) {
             fread(&number, sizeof(int), 1, binary_file);
             printf("%4d", number);
         }
@@ -48,20 +48,21 @@ void print_file(char* name, int n){
     printf("\n");
     fclose(binary_file);
 }
-int column_sum(int **x, int n, int c){
+
+int column_sum(int **x, int n, int c) {
     int csum = 0;
     for (int i = 0; i < n; i++)
         csum += x[i][c];
     return csum;
 }
 
-int main(){
+int main() {
     int n, column;
     printf("Enter matrix size: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int **x = binary_file_w_matrix("a.txt", n);
     print_file("a.txt", n);
     printf("Enter column number to sum: ");
-    scanf("%d",&column);
+    scanf("%d", &column);
     printf("%d", column_sum(x, n, column));
 }
