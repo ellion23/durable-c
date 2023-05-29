b13b0417104716752d7db174356d746335ae8b00
ellion23 <rubcinskija@gmail.com>
2023-05-29 10:44:30 +0700

    upd

--- practice/p2-structures.c
+++ practice/p2-structures.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 //  По индивидуальному варианту написать функции:
 //  1.	Считывания данных из текстового файла в массив (В текстовом файле должно находиться не менее 20 записей)
@@ -21,14 +22,39 @@ typedef struct  {
 } export_data;
 
 
-void read_file(FILE* file, export_data* data) {
+int read_file(FILE* file, export_data* data) {
     int i = 0;
     while (!feof(file)) {
         fscanf(file, "%s %s %d", data[i].country, data[i].name, &data[i].export_volume);
-        printf("%s %s %d \n", data[i].country, data[i].name, data->export_volume);
+//        printf("%s %s %d \n", data[i].country, data[i].name, data[i].export_volume);
+        i++;
     }
+    return i;
 }
 
+void find(export_data* data, int n) {
+    char name[50];
+    int exp;
+    printf("Введите наименование товара и объём экспорта: ");
+    int check = scanf("%s %d", name, &exp);
+    if (check != 2) {
+        printf("Неверный ввод\n");
+        exit(1);
+    }
+    int flag = 0;
+    for (int i = 0; i < n; i++) {
+        check = strcmp(name, data[i].name);
+
+        if (!check && exp == data[i].export_volume) {
+            printf("%s %s %d\n", data[i].country, data[i].name, data[i].export_volume);
+            flag = 1;
+        }
+    }
+    if (!flag) {
+        printf("Данных не найдено\n");
+    }
+
+}
 
 int main() {
     char* filename = "../practice/p2.txt";
@@ -38,8 +64,8 @@ int main() {
         return 1;
     }
 
-    export_data data[20];
-    read_file(file, data);
-
+    export_data data[50];
+    int count = read_file(file, data);
+    find(data, count);
     return 0;
 }
