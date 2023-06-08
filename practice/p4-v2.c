621df64af647cf078531674efb60958d99c5fd33
ellion23 <70380004+ellion23@users.noreply.github.com>
2023-06-08 08:22:09 +0700

    upd

---
+++ practice/p4-v2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double f(double x, int equation) {
+    switch (equation) {
+        case 1:
+            return pow(x, 4) - 0.48 * pow(x, 3) - 5.792 * pow(x, 2) + 0.486 * x + 4.792; // -2, -1, 1, 2
+        case 2:
+            return 0.1 * sin(x) + pow(x, 3) - 1;  // 1
+        case 3:
+            return 0.1 * exp(x) - pow(sin(x), 2) + 0.5; // -1 в периоде, 1; -4 -2.5 -1 1 1.5
+    }
+    printf("Неверный номер уравнения.");
+    exit(0);
+}
+
+double df(double x, int equation) {
+    switch (equation) {
+        case 1:
+            return 4 * pow(x, 3) - 36 * pow(x, 2) / 25 - 1448 * x / 125 + 243. / 500;
+        case 2:
+            return 3 * x * x + 0.1 * cos(x);
+        case 3:
+            return 0.1 * exp(x) - 2 * sin(x) * cos(x);
+    }
+}
+
+double method_kasatelnih(double x0, double epsilon, int equation) {
+    double x = x0;
+    int iter = 0;
+    double delta = fabs(f(x, equation) / df(x, equation));
+
+    while (delta >= epsilon) {
+        x = x - f(x, equation) / df(x, equation);
+        delta = fabs(f(x, equation) / df(x, equation));
+        iter++;
+    }
+
+        printf("Корень: %.5lf, найденный за %d итераций", x, iter);
+
+
+    return x;
+}
+
+void check_input(int a, int b) {
+    if (a != b) {
+        printf("Некорректный ввод");
+        exit(0);
+    }
+}
+
+int main() {
+    system("chcp 65001");
+    double x0;
+    double epsilon;
+    int eq;
+    printf("Введите номер уравнения: ");
+    int check = scanf("%d", &eq);
+    check_input(check, 1);
+    printf("Введите приближённое значение и точность: ");
+    check = scanf("%lf %lf", &x0, &epsilon);
+    check_input(check, 2);
+    method_kasatelnih(x0, epsilon, eq);
+    return 0;
+}
--- practice/p4.c
+++ practice/p4.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 
+int vall;
 
 double func(double x) {
     // Здесь задаем функцию, корень которой мы ищем
@@ -11,10 +12,16 @@ double func(double x) {
 //    return 0.1*exp(x) - pow(sin(x), 2) + 0.5; // -1 в периоде, 1
 }
 
+Алгоритм метода.
+1. Определяем по графику приближённое значение корня
+
+x^4 - 0.48 * x^3 - 5.792 * x^2 + 0.486 * x + 4.792
+0.1 * sin(x) + x^3 - 1
+0.1 * exp(x) - (sin(x))^2 + 0.5
 
 double proisvodnaya(double x) {
     // Здесь задаем производную функции
-    return 4*pow(x, 3) - 36*pow(x, 2)/25 - 1448*x/125 + 243 / 500;
+    return 4*pow(x, 3) - 36*pow(x, 2)/25 - 1448*x/125 + 243. / 500;
 //    return 3*x*x + 0.1*cos(x);
 //    return 0.1*exp(x) - 2*sin(x)*cos(x);
 }
@@ -25,21 +32,29 @@ double method_kasatelnyh(double x0, double epsilon) {
     while(fabs(x1 - x0) > epsilon) {
         x0 = x1;
         x1 = x0 - func(x0) / proisvodnaya(x0);
+        vall++;
     }
     return x1;
 }
 
 
 int main() {
-    double x0;   // Задаем начальное приближение
-    double epsilon;     // Задаем точность
-    printf("Введите начальное приближение и точность вычислений: ");
-    int check = scanf("%lf %lf", &x0, &epsilon);
-    if (check != 2) {
-        printf("Некорректный ввод\n");
-        exit(0);
+    system("chcp 65001");
+    double x0;
+    double epsilon;
+    x0 = 2.;
+    epsilon = 10.;
+    double root;
+
+    for (int i = 0; i < 7; i++) {
+        printf("-----my-----\n");
+        epsilon /= 10;
+        vall = 0;
+        root = method_kasatelnyh(x0, epsilon);
+        printf("Корень: %lf Итерации: %d      | %lf\n", root, vall, epsilon);
+        printf("-not-my-----\n");
+        printf("\n");
     }
-    double root = method_kasatelnyh(x0, epsilon);   // Находим корень
-    printf("Корень: %lf\n", root);
+
     return 0;
 }
