32276f9c30e56d643669e8e624e43d8efbea37a7
ellion23 <rubcinskija@gmail.com>
2023-06-19 06:22:56 +0700

    upd

--- lab/lab4-2.c
+++ lab/lab4-2.c
@@ -12,13 +12,6 @@ void swap(int *a, int *b) // example of function call: swap(&x, &y);
 }
 
 
-void print_array(int array[], const int n) // print array to std output; array, length
-{
-    for (int i = 0; i < n; i++) printf("%5d ", array[i]);
-    printf("\n");
-}
-
-
 void insertion_sort(int array[], int n, int *count) {
     int element, index, i;
     index = 0;
@@ -43,8 +36,16 @@ void insertion_sort(int array[], int n, int *count) {
     }
 }
 
+
+void print_array(int array[], const int n) // print array to std output; array, length
+{
+    for (int i = 0; i < n; i++) printf("%5d ", array[i]);
+    printf("\n");
+}
+
+
 void fill_array(int array[], const int n) {
-    int maxvalue = 100;
+    int maxvalue = 10000;
     int maxvalue2 = maxvalue * 2;
     for (int i = 0; i < n; i++) {
         array[i] = rand() % maxvalue2 - maxvalue;
@@ -53,23 +54,26 @@ void fill_array(int array[], const int n) {
 
 
 int main() {
-    int count = 0;
-    int n;
-    printf("Введите размерность массива: ");
-    int check = scanf("%d", &n);
-    if (!check || n <= 0) {
-        printf("Input error\n");
-        exit(2);
-    }
     srand(time(NULL));
-    int *array = malloc(n * sizeof(int));
-    printf("До:\n");
-    fill_array(array, n);
-    print_array(array, n);
-    printf("После:\n");
-    insertion_sort(array, n, &count);
-    print_array(array, n);
-    printf("%5d\n", count);
+    int *array;
+    int count;
+    int i;
+    int all;
+    FILE *f = fopen("../lab/data2.csv", "w");
+    for (int n = 100; n <= 10000; n += 100) {
+        all = 0;
+        for (i = 0; i < 40; i++) {
+            count = 0;
+            array = malloc(n * sizeof(int));
+            fill_array(array, n);
+            insertion_sort(array, n, &count);
+            free(array);
+            all += count;
+        }
+        count = all / i;
+        fprintf(f, "%d; %d\n", n, count);
+    }
 
+    fclose(f);
     return 0;
 }
